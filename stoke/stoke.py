a39662eb787c929155b50cefe7627e1b5c2fe187
Nicholas Cilfone <nicholas.cilfone@fmr.com>
2021-08-03 13:15:53 -0400

    rescale the step loss back to true loss

--- stoke/stoke.py
+++ stoke/stoke.py
@@ -1172,8 +1172,8 @@ class Stoke:
 
     @property
     def step_loss(self):
-        """Gets the step loss"""
-        return self._scale_agg_loss()
+        """Gets the step loss -- rescaled"""
+        return self._scale_agg_loss() * self.grad_accum
 
     @property
     def model_access(self):
